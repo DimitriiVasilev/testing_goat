20dc0295f2321dedaf23a4d9e7a503e5fdfc2883
DimitriiVasilev <duscha1997@mail.ru>
2019-06-02 11:40:10 +0300

    view returns minimal html

--- superlist/functional_tests.py
+++ superlist/functional_tests.py
@@ -17,7 +17,7 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.implicitly_wait(3)
 
         # She notices the page title and header mention to-do lists
-        self.assertIn('To-do', self.browser.title)
+        self.assertIn('To-Do', self.browser.title)
         self.fail('Finish the test')
 
         # She is invited to enter a to-do item straight away

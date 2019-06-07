752521863ec1ca6b93b6080f61391149674ed468
DimitriiVasilev <duscha1997@mail.ru>
2019-06-07 21:30:01 +0300

    waiting for spare time

--- superlist/lists/tests.py
+++ superlist/lists/tests.py
@@ -2,8 +2,14 @@ from django.test import TestCase
 from django.urls import resolve
 from django.http import HttpRequest
 from django.template.loader import render_to_string
+import re
 
 from .views import home_page
+
+
+def remove_csrf(html_code):
+    csrf_regex = r'<input[^>]+csrfmiddlewaretoken[^>]+>'
+    return re.sub(csrf_regex, '', html_code)
 
 
 class HomePageTest(TestCase):
@@ -15,5 +21,15 @@ class HomePageTest(TestCase):
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
-        self.assertEqual(response.content.decode(), expected_html)
+        expected_html = render_to_string('home.html', request=request)
+        self.assertEqual(
+            remove_csrf(response.content.decode()),
+            remove_csrf(expected_html)
+        )
+
+    def test_home_page_can_save_a_POST_request(self):
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+        response = home_page(request)
+        self.assertIn('A new list item', response.content.decode())

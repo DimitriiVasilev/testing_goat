30a1c92d8e6af1676d98a853dc1b5fd127cab45f
DimitriiVasilev <duscha1997@mail.ru>
2019-06-16 17:17:14 +0300

    Refactor URLs

--- superlist/lists/urls.py
+++ superlist/lists/urls.py
@@ -3,10 +3,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home_page, name='home_page'),
-    path('lists/', include([
-        path('<int:list_id>/', views.view_list, name='view_list'),
-        path('<int:list_id>/add_item', views.add_item, name='add_item'),
-        path('new', views.new_list, name='new_list')
-    ])),
+    path('<int:list_id>/', views.view_list, name='view_list'),
+    path('<int:list_id>/add_item', views.add_item, name='add_item'),
+    path('new', views.new_list, name='new_list')
 ]
